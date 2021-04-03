85e6b2b65b5b5571dce92759a31e70165de55dde
eilla1 <exu6056@gmail.com>
2021-04-02 22:15:31 -0700

    fuck i broke it

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import HeadObject from '../components/head'
 import Nav from '../components/nav';
 import { BsChevronCompactDown } from 'react-icons/bs';
 import Footer from '../components/footer';
+import getMentors from '../lib/getMentors';
 
-export default function Home() {
+getMentors();
+
+export default function Home({ mentors }) {
   useEffect(() => {
     console.log('asdfasdf')
   });
@@ -18,7 +21,7 @@ export default function Home() {
         <section>
           <div className="text-center">
             <p className="text-gray-400 text-xl font-montserrat font-semibold">command tech presents</p>
-            <h1 className="text-5xl font-extrabold font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-[#C67DFF] to-[#497CFF] text-shadow-lg text-shadow-blue dark:text-shadow-white">Command Connect</h1>
+            <h1 className="text-5xl font-extrabold font-montserrat bg-clip-text text-transparent bg-gradient-to-r from-[#C67DFF] to-[#497CFF] text-shadow-lg dark:text-shadow-white">Command Connect</h1>
             <h3 className="text-lg md:text-xl font-openSans mt-2 md:w-4/5 mx-auto">A sentence that briefly summarises Command Connect in like 2 lines I think. Nah I need another sentence help help help.</h3>
             <Link href="#about">
               <BsChevronCompactDown size={50} color="gray" className="mx-auto"/>
@@ -32,4 +35,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const mentors = await getMentors();
+
+  return {
+    props: {
+      mentors,
+    },
+  };
 }
