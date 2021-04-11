611e65a38e4fc8de172eac3f8bdcf1ebf08b06a9
eilla1 <exu6056@gmail.com>
2021-04-11 00:37:58 -0700

    chore: fix everything
    I deleted the afeat: fix everything
    
    I deleted the apply page and replaced it with 2 buttons in the
    hero linking to the airtable forms. I also did some small ui fix
    es and added real stuff for the testimonials component
    
    this commit closes #12

--- components/testimonial.js
+++ components/testimonial.js
@@ -3,10 +3,10 @@ import { ImQuotesLeft } from "react-icons/im";
 export default function Testimonial({ quote, profileIcon, name, role }) {
   return (
     <div>
-      <div className="w-72">
-        <div className="flex flex-row space-x-3 space-y-10">
-          <ImQuotesLeft size={90} />
-          <p className="italic">{quote}</p>
+      <div className="w-72 align-middle">
+        <div>
+          {/* <ImQuotesLeft size={90} /> */}
+          <p className="italic text-center md:text-left">{quote}</p>
         </div>
         <div className="flex flex-row space-x-3">
           <img src={profileIcon} alt={name} className="rounded-full w-8 h-8" />
